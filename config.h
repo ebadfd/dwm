180b052cd79f718a5ca5c30273fcb24f63c5a49f
dasith <dasith@ebadfd.tech>
2024-11-29 20:06:26 +0530

    fix: updated color scheme and set themes

--- config.h
+++ config.h
@@ -29,12 +29,6 @@ static const char *colors[][3] = {
     [SchemeSel] = {gray4, blue, arapawa_blue},
 };
 
-static const char col_gray1[] = "#222222";
-static const char col_gray2[] = "#444444";
-static const char col_gray3[] = "#bbbbbb";
-static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[] = "#005577";
-
 /* tagging */
 static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
@@ -81,8 +75,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
-    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+    "dmenu_run", "-m",     dmenumon, "-fn", dmenufont, "-nb",       black,
+    "-nf",       luckydog, "-sb",    blue,  "-sf",     grayishblue, NULL};
 static const char *termcmd[] = {"alacritty", NULL};
 
 static const Key keys[] = {
