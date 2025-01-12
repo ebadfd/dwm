99a9a45df18ec3b117f37298ab129f5e37278c3f
dasith <dasith@ebadfd.tech>
2025-01-12 18:07:38 +0530

    fix systray icons

--- dwm.c
+++ dwm.c
@@ -1571,9 +1571,9 @@ resizerequest(XEvent *e)
 	Client *i;
 
 	if ((i = wintosystrayicon(ev->window))) {
-		updatesystrayicongeom(i, ev->width, ev->height);
-		resizebarwin(selmon);
-		updatesystray();
+                updatesystrayicongeom(i, ev->width, ev->height);
+                resizebarwin(selmon);
+                updatesystray();
 	}
 }
 
@@ -2350,22 +2350,10 @@ void
 updatesystrayicongeom(Client *i, int w, int h)
 {
 	if (i) {
-		i->h = bh;
-		if (w == h)
-			i->w = bh;
-		else if (h == bh)
-			i->w = w;
-		else
-			i->w = (int) ((float)bh * ((float)w / (float)h));
-		applysizehints(i, &(i->x), &(i->y), &(i->w), &(i->h), False);
-		/* force icons into the systray dimensions if they don't want to */
-		if (i->h > bh) {
-			if (i->w == i->h)
-				i->w = bh;
-			else
-				i->w = (int) ((float)bh * ((float)i->w / (float)i->h));
-			i->h = bh;
-		}
+                i->w = w = 16; // Set icon width to 16
+                i->h = h = 16; // Set icon height to 16
+                    i->y = ((bh - h) / 2); // Calculate y to center icon vertically
+                XMoveResizeWindow(dpy, i->win, i->x, i->y, i->w, i->h);
 	}
 }
 
@@ -2407,11 +2395,12 @@ updatesystray(void)
 	unsigned int x = m->mx + m->mw;
 	unsigned int sw = TEXTW(stext) - lrpad + systrayspacing;
 	unsigned int w = 1;
+	unsigned int y = ((bh - 20) / 2);
 
 	if (!showsystray)
 		return;
 	if (systrayonleft)
-		x -= sw + lrpad / 3;
+		x -= sw + lrpad / 2;
 	if (!systray) {
 		/* init systray */
 		if (!(systray = (Systray *)calloc(1, sizeof(Systray))))
@@ -2444,7 +2433,8 @@ updatesystray(void)
 		XMapRaised(dpy, i->win);
 		w += systrayspacing;
 		i->x = w;
-		XMoveResizeWindow(dpy, i->win, i->x, 0, i->w, i->h);
+		i->y = y;
+		XMoveResizeWindow(dpy, i->win, i->x, i->y, i->w, i->h);
 		w += i->w;
 		if (i->mon != m)
 			i->mon = m;
