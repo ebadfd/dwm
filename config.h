6cb2645a0ca5e11aa6e79d0d715c8fe62792f3e0
dasith <dasith@ebadfd.tech>
2025-01-12 19:30:47 +0530

    patch: revert systray and apply status2d bar padding with systray

--- config.h
+++ config.h
@@ -15,6 +15,9 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray             = 1;        /* 0 means no systray */
 
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
+
 #include "themes/cozybear.h"
 
 static const char *colors[][3]      = {
