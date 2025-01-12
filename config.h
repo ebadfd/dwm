70f5294af566f7786956983e0584c60abde2399d
dasith <dasith@ebadfd.tech>
2025-01-12 22:14:48 +0530

    chore: updated the gapps and vertical bar padding

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappx     = 5;        /* gap pixel between windows */
+static const unsigned int gappx     = 4;        /* gap pixel between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"FiraMono Nerd Font:style=Regular:size=10"};
